59129cea2aaf3e573426bd16846e83146a074a18
Ambrose <152869@cognizant.com>
2018-04-02 15:51:01 -0500

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './images/login.jpg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <img src={logo} alt="logo" />
+      </div>
+    );
+  }
+
+}
+export default App;
+
